fc679c27f8be1185c2314985e8a7392c09a21ee5
Guido Lorenz <guido@doo.net>
2012-04-12 10:57:36 +0200

    Refactored SQLite3.js
    - "use strict"
    - Use WinJS.Class.define
    - Added Statement class

--- SQLite3js/js/SQLite3.js
+++ SQLite3js/js/SQLite3.js
@@ -1,74 +1,82 @@
 ﻿(function () {
+  "use strict";
 
-  function Database(dbPath) {
+  var Statement = WinJS.Class.define(function (statement) {
+    this.statement = statement;
+  }, {
+    bindArgs: function (args) {
+      var i, len, arg, index, resultCode;
+
+      if (args) {
+        for (i = 0, len = args.length; i < len; i++) {
+          arg = args[i];
+          index = i + 1;
+          switch (typeof arg) {
+            case 'number':
+              if (arg === +arg && arg === (arg | 0)) {
+                resultCode = this.statement.bindInt(index, arg);
+              } else {
+                resultCode = this.statement.bindDouble(index, arg);
+              }
+              break;
+            case 'string':
+              resultCode = this.statement.bindText(index, arg);
+              break;
+            default:
+              throw new Error("Unsupported argument type.");
+          }
+          if (resultCode !== SQLite3.ResultCode.ok) {
+            throw new Error("Error " + resultCode + " when binding argument to SQL query.");
+          }
+        }
+      }
+    },
+    execute: function () {
+      var result = [], row, i, len, name;
+
+      while (this.statement.step() === SQLite3.ResultCode.row) {
+        row = {};
+        for (i = 0, len = this.statement.columnCount() ; i < len; i++) {
+          name = this.statement.columnName(i);
+          switch (this.statement.columnType(i)) {
+            case SQLite3.Datatype.integer:
+              row[name] = this.statement.columnInt(i);
+              break;
+            case SQLite3.Datatype.float:
+              row[name] = this.statement.columnDouble(i);
+              break;
+            case SQLite3.Datatype.text:
+              row[name] = this.statement.columnText(i);
+              break;
+            case SQLite3.Datatype["null"]:
+              row[name] = null;
+          }
+        }
+        result.push(row);
+      }
+      return result;
+    },
+    close: function () {
+      this.statement.close();
+    }
+  });
+
+  var Database = WinJS.Class.define(function (dbPath) {
     this.db = SQLite3.Database(dbPath);
-  }
+  }, {
+    execute: function (sql, args) {
+      var statement, rows;
 
-  Database.prototype.execute = function (sql, args) {
-    var statement;
-
-    statement = this.db.prepare(sql);
-    bindArgs(statement, args);
-    return executeStatement(statement);
-  };
-
-  Database.prototype.close = function () {
-    this.db.close();
-  };
-
-  function bindArgs(statement, args) {
-    var i, len, arg, index, resultCode;
-
-    if (args) {
-      for (i = 0, len = args.length; i < len; i++) {
-        arg = args[i];
-        index = i + 1;
-        switch (typeof arg) {
-          case 'number':
-            if (arg === +arg && arg === (arg | 0)) {
-              resultCode = statement.bindInt(index, arg);
-            } else {
-              resultCode = statement.bindDouble(index, arg);
-            }
-            break;
-          case 'string':
-            resultCode = statement.bindText(index, arg);
-            break;
-          default:
-            throw new Error("Unsupported argument type.");
-        }
-        if (resultCode !== SQLite3.ResultCode.ok) {
-          throw new Error("Error " + resultCode + " when binding argument to SQL query.");
-        }
-      }
+      statement = new Statement(this.db.prepare(sql));
+      statement.bindArgs(args);
+      rows = statement.execute();
+      statement.close();
+      return rows;
+    },
+    close: function () {
+      this.db.close();
     }
-  };
-
-  function executeStatement(statement) {
-    var result = [], row, i, len, name;
-
-    while (statement.step() === SQLite3.ResultCode.row) {
-      row = {};
-      for (i = 0, len = statement.columnCount() ; i < len; i++) {
-        name = statement.columnName(i);
-        switch (statement.columnType(i)) {
-          case SQLite3.Datatype.integer:
-            row[name] = statement.columnInt(i);
-            break;
-          case SQLite3.Datatype.float:
-            row[name] = statement.columnDouble(i);
-            break;
-          case SQLite3.Datatype.text:
-            row[name] = statement.columnText(i);
-            break;
-          case SQLite3.Datatype["null"]:
-            row[name] = null;
-        }
-      }
-      result.push(row);
-    }
-    return result;
-  };
+  });
 
   WinJS.Namespace.define('SQLite3js', {
     Database: Database
