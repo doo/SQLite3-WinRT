e5f7c3b5b8784d93cf21d2356c8da5544b6b037c
Guido Lorenz <guido@doo.net>
2012-04-12 10:34:33 +0200

    Added JavaScript wrapper and specs

---
+++ SQLite3js/js/SQLite3.js
@@ -0,0 +1,77 @@
+﻿(function () {
+
+  function Database(dbPath) {
+    this.db = SQLite3.Database(dbPath);
+  }
+
+  Database.prototype.execute = function (sql, args) {
+    var statement;
+
+    statement = this.db.prepare(sql);
+    bindArgs(statement, args);
+    return executeStatement(statement);
+  };
+
+  Database.prototype.close = function () {
+    this.db.close();
+  };
+
+  function bindArgs(statement, args) {
+    var i, len, arg, index, resultCode;
+
+    if (args) {
+      for (i = 0, len = args.length; i < len; i++) {
+        arg = args[i];
+        index = i + 1;
+        switch (typeof arg) {
+          case 'number':
+            if (arg === +arg && arg === (arg | 0)) {
+              resultCode = statement.bindInt(index, arg);
+            } else {
+              resultCode = statement.bindDouble(index, arg);
+            }
+            break;
+          case 'string':
+            resultCode = statement.bindText(index, arg);
+            break;
+          default:
+            throw new Error("Unsupported argument type.");
+        }
+        if (resultCode !== SQLite3.ResultCode.ok) {
+          throw new Error("Error " + resultCode + " when binding argument to SQL query.");
+        }
+      }
+    }
+  };
+
+  function executeStatement(statement) {
+    var result = [], row, i, len, name;
+
+    while (statement.step() === SQLite3.ResultCode.row) {
+      row = {};
+      for (i = 0, len = statement.columnCount() ; i < len; i++) {
+        name = statement.columnName(i);
+        switch (statement.columnType(i)) {
+          case SQLite3.Datatype.integer:
+            row[name] = statement.columnInt(i);
+            break;
+          case SQLite3.Datatype.float:
+            row[name] = statement.columnDouble(i);
+            break;
+          case SQLite3.Datatype.text:
+            row[name] = statement.columnText(i);
+            break;
+          case SQLite3.Datatype["null"]:
+            row[name] = null;
+        }
+      }
+      result.push(row);
+    }
+    return result;
+  };
+
+  WinJS.Namespace.define('SQLite3js', {
+    Database: Database
+  });
+
+})();
---
+++ SQLite3js/spec/SQLite3Spec.js
@@ -0,0 +1,42 @@
+﻿describe('SQLite3js', function () {
+  var db = null;
+
+  beforeEach(function () {
+    db = new SQLite3js.Database(':memory:');
+    db.execute('CREATE TABLE Item (name TEXT, price REAL, id INT PRIMARY KEY)');
+    db.execute('INSERT INTO Item (name, price, id) VALUES (?, ?, ?)', ['Apple', 1.2, 1]);
+    db.execute('INSERT INTO Item (name, price, id) VALUES (?, ?, ?)', ['Orange', 2.5, 2]);
+    db.execute('INSERT INTO Item (name, price, id) VALUES (?, ?, ?)', ['Banana', 3, 3]);
+  });
+
+  afterEach(function () {
+    db.execute('DROP TABLE Item');
+    db.close();
+  });
+  
+  it('should return the correct count', function () {
+    var rows;
+
+    rows = db.execute('SELECT COUNT(*) AS count FROM Item');
+    return expect(rows[0].count).toEqual(3);
+  });
+
+  it('should return an item by id', function () {
+    var rows;
+
+    rows = db.execute('SELECT * FROM Item WHERE id = ?', [2]);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].name).toEqual('Orange');
+    expect(rows[0].price).toEqual(2.5);
+    expect(rows[0].id).toEqual(2);
+  });
+
+  it('should return items with names ending on "e"', function () {
+    var expectedValues, i, properties, property, rows, _i, _len, _ref;
+
+    rows = db.execute('SELECT * FROM Item WHERE name LIKE ? ORDER BY id ASC', ['%e']);
+    expect(rows.length).toEqual(2);
+    expect(rows[0].name).toEqual('Apple');
+    expect(rows[1].name).toEqual('Orange');
+  });
+});
