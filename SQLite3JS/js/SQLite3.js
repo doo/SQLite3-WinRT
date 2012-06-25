0d5c0108af33a7f0d28e77b0b6af697205e9bbcd
Guido Lorenz <guido@doo.net>
2012-06-25 10:48:11 +0200

    Removed JS Statement class
    The statement functionality is now implemented in C++ in SQLite3::Statement.
    For now, statements cannot be used directly in JavaScript, only implicitly
    using Database::runAsync() etc.

--- SQLite3JS/js/SQLite3.js
+++ SQLite3JS/js/SQLite3.js
@@ -1,7 +1,7 @@
 ﻿(function () {
   "use strict";
 
-  var Statement, Database, ItemDataSource, GroupDataSource;
+  var Database, ItemDataSource, GroupDataSource;
 
   // Alternative typeof implementation yielding more meaningful results,
   // see http://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
@@ -17,97 +17,6 @@
     error.resultCode = comException.number & 0xffff;
     return error;
   }
-
-  Statement = WinJS.Class.define(function (statement, args) {
-    this.statement = statement;
-
-    if (args) {
-      this.bind(args);
-    }
-  }, {
-    bind: function (args) {
-      var index, resultCode;
-
-      args.forEach(function (arg, i) {
-        index = i + 1;
-        switch (type(arg)) {
-          case 'number':
-            if (arg % 1 === 0) {
-              resultCode = this.statement.bindInt(index, arg);
-            } else {
-              resultCode = this.statement.bindDouble(index, arg);
-            }
-            break;
-          case 'string':
-            resultCode = this.statement.bindText(index, arg);
-            break;
-          case 'null':
-            resultCode = this.statement.bindNull(index);
-            break;
-          default:
-            throw new Error("Unsupported argument type: " + type(arg));
-        }
-        if (resultCode !== SQLite3.ResultCode.ok) {
-          throw new Error("Error " + resultCode + " when binding argument to SQL query.");
-        }
-      }, this);
-    },
-    run: function () {
-      this.statement.step();
-    },
-    one: function () {
-      this.statement.step();
-      return this._getRow();
-    },
-    all: function () {
-      var result = [];
-
-      this.each(function (row) {
-        result.push(row);
-      });
-      return result;
-    },
-    each: function (callback) {
-      while (this.statement.step() === SQLite3.ResultCode.row) {
-        callback(this._getRow());
-      }
-    },
-    map: function (callback) {
-      var result = [];
-
-      this.each(function (row) {
-        result.push(callback(row));
-      });
-      return result;
-    },
-    close: function () {
-      this.statement.close();
-    },
-    _getRow: function () {
-      var i, len, name, row = {};
-
-      for (i = 0, len = this.statement.columnCount() ; i < len; i += 1) {
-        name = this.statement.columnName(i);
-        row[name] = this._getColumn(i);
-      }
-
-      return row;
-    },
-    _getColumn: function (index) {
-      switch (this.statement.columnType(index)) {
-        case SQLite3.Datatype.integer:
-          return this.statement.columnInt(index);
-        case SQLite3.Datatype.float:
-          return this.statement.columnDouble(index);
-        case SQLite3.Datatype.text:
-          return this.statement.columnText(index);
-        case SQLite3.Datatype["null"]:
-          return null;
-        default:
-          throw new Error('Unsupported column type in column ' + index);
-      }
-    }
-  });
 
   Database = WinJS.Class.define(function (connection) {
     this.connection = connection;
@@ -155,14 +64,6 @@
         results.push(callback(row));
       }).then(function () {
         return results;
-      });
-    },
-    prepareAsync: function (sql, args) {
-      return this.connection.prepareAsync(sql).then(function (statement) {
-        return new Statement(statement, args);
-      }, function (error) {
-        var sqliteError = toSQLiteError(error, 'Error preparing an SQLite statement.');
-        return WinJS.Promise.wrapError(sqliteError);
       });
     },
     itemDataSource: function (sql, args, keyColumnName, groupKeyColumnName) {
