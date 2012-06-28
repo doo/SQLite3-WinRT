aa64de46a1f27e74c946298643adad6093576bdf
Guido Lorenz <guido@doo.net>
2012-06-28 10:26:08 +0200

    Add SQLite result code to error messages

--- SQLite3JS/js/SQLite3.js
+++ SQLite3JS/js/SQLite3.js
@@ -30,9 +30,11 @@
   }
 
   function wrapComException(comException) {
+    var resultCode = comException.number & 0xffff;
+
     return WinJS.Promise.wrapError({
-      message: 'SQLite Error',
-      resultCode: comException.number & 0xffff
+      message: 'SQLite Error (Result Code ' + resultCode + ')',
+      resultCode: resultCode
     });
   }
 
