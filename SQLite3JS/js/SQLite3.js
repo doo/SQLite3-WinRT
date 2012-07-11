793e17c8407df94d9023e432ab5e6c463711706b
Marcus Ilgner <marcus@doo.net>
2012-07-11 16:51:51 +0200

    Performance: use PropertySet iterator for toObjectImpl

--- SQLite3JS/js/SQLite3.js
+++ SQLite3JS/js/SQLite3.js
@@ -12,19 +12,18 @@
     return typeString.substring(8, typeString.length - 1).toLowerCase();
   }
 
-  function toObject(propertySet) {
-    function toObjectImpl(propertySet) {
-      var key, object = {};
+  function toObjectImpl(propertySet) {
+    var key, object = {}, iterator = propertySet.first();
 
-      for (key in propertySet) {
-        if (propertySet.hasOwnProperty(key)) {
-          object[key] = propertySet[key];
-        }
-      }
-
-      return object;
+    while (iterator.hasCurrent) {
+      object[iterator.current.key] = iterator.current.value;
+      iterator.moveNext();
     }
 
+    return object;
+  }
+
+  function toObject(propertySet) {
     return propertySet ? toObjectImpl(propertySet) : null;
   }
 
@@ -183,7 +182,7 @@
                   offset: countBefore,
                   totalCount: totalCount
                 };
-              })
+              });
           });
         },
         setNotificationHandler: function (notificationHandler) {
@@ -248,6 +247,7 @@
               items: dataAdapter._groups.slice(),
               offset: requestIndex,
               absoluteIndex: requestIndex,
+              totalCount: dataAdapter._groups.length,
               atStart: true,
               atEnd: true
             };
