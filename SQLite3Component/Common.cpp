78702be1ab4701d52143a10491fcc8ec3ad56d8f
Guido Lorenz <guido@doo.net>
2012-11-15 09:57:28 +0100

    Use default parameters for length in string conversion functions

--- SQLite3Component/Common.cpp
+++ SQLite3Component/Common.cpp
@@ -9,10 +9,6 @@ namespace SQLite3 {
     throw ref new Platform::COMException(hresult);
   }
 
-  std::wstring ToWString(const char* utf8String) {
-    return ToWString(utf8String, -1);
-  }
-
   std::wstring ToWString(const char* utf8String, unsigned int length) {
     DWORD numCharacters = MultiByteToWideChar(CP_UTF8, 0, utf8String, length, nullptr, 0);
     auto wideText = new std::wstring::value_type[numCharacters];
@@ -24,9 +20,5 @@ namespace SQLite3 {
 
   Platform::String^ ToPlatformString(const char* utf8String, unsigned int length) {
     return ref new Platform::String(ToWString(utf8String, length).data());
-  }
-
-  Platform::String^ ToPlatformString(const char* utf8String) {
-    return ref new Platform::String(ToWString(utf8String, -1).data());
   }
 }
